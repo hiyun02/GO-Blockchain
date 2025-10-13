3de3c202c0fed621515cdef58e49022c29398574
hiyun02 <psalter150@naver.com>
2025-10-13 21:09:25 +0900

    Merkle Proof 추가

--- ott/block.go
+++ ott/block.go
@@ -7,34 +7,37 @@ import (
 	"time"
 )
 
-// BlockHeader : 블록의 메타데이터
+// 블록의 메타데이터
 type BlockHeader struct {
 	Index     int    `json:"index"`     // 블록 번호
 	Timestamp string `json:"timestamp"` // 생성 시간
 	PrevHash  string `json:"prev_hash"` // 이전 블록의 해시
+	MerkleRoot string `json:"merkle_root"` // 블록 내부 데이터의 요약 해시(머클루트)
 	Hash      string `json:"hash"`      // 현재 블록의 해시
 	Nonce     int    `json:"nonce"`     // 작업증명에 사용된 값
 }
 
-// BlockPayload : 블록에 들어가는 실제 데이터
-// OTT/CP 데이터는 나중에 여기에 확장해서 넣으면 됨
-type BlockPayload struct {
+// 머클트리용 컨텐츠 구조
+type ContentRecord struct {
+	ContentID string `json:"content_id"`
 	Data string `json:"data"`
+	Timestamp string `json:"timestamp"`
 }
 
-// Block : 하나의 블록 = Header + Payload
+// B하나의 블록 = Header + Entries
+// 기존 Payload 구조 대신 여러 컨텐츠를 담는 형태
 type Block struct {
 	Header  BlockHeader  `json:"header"`
-	Payload BlockPayload `json:"payload"`
+	Entries []ContentRecord `json:"entries"`
 }
 
-// calculateHash : 블록의 해시값 계산
-// Index, Timestamp, Data, PrevHash, Nonce 를 모두 연결해서 SHA-256 해시 생성
+// 블록의 해시값 계산
+// Index, Timestamp, Data, PrevHash, Nonce, MerkleRoot 를 모두 이용하여 SHA-256 해시 생성
 func calculateHash(block Block) string {
 	record := strconv.Itoa(block.Header.Index) +
 		block.Header.Timestamp +
-		block.Payload.Data +
 		block.Header.PrevHash +
+		block.Header.MerkleRoot +
 		strconv.Itoa(block.Header.Nonce)
 
 	h := sha256.New()
@@ -43,18 +46,23 @@ func calculateHash(block Block) string {
 	return hex.EncodeToString(hashed)
 }
 
-// newBlock : 새 블록을 만드는 기본 함수 (PoW 적용은 blockchain.go에서 처리)
+// 새 블록 생성 함수 (PoW 적용은 blockchain.go에서 처리)
 func newBlock(index int, prevHash string, data string) Block {
+	entry := ContentRecord{
+		ContentID: "rec_" + strconv.Itoa(index),
+		Data :	data,
+		Timestamp: time.Now().Format(time.RFC3339),
+
+
 	block := Block{
 		Header: BlockHeader{
 			Index:     index,
-			Timestamp: time.Now().String(),
+			Timestamp: time.Now().Format(time.RFC3339),
 			PrevHash:  prevHash,
-			Nonce:     0,
+			MerkleRoot: "" // Merkle 계산 후에 업데이트
+			Nonce:     0, // Nonce 계산 후에 업데이트
 		},
-		Payload: BlockPayload{
-			Data: data,
-		},
+		Entries: []ContentRecord{entry},
 	}
 	return block
 }
